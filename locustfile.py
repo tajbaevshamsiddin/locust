10a75cff2426382573be7af7e47f11305b1ecbe1
tajbaevshamsiddin <tajbaevsh@gmail.com>
2023-04-25 19:52:40 +0500

    v2

--- locustfile.py
+++ locustfile.py
@@ -10,4 +10,24 @@ class HelloWorldUser(HttpUser):
     def hello_world(self): 
         # This method will run an HTTP GET request on the path `/` 
         # of the site you are testing
-        self.client.get("/") 
+        self.client.post("/todos/", {
+            
+        }) 
+
+# class WebsiteUser(HttpUser):
+#     wait_time = between(5, 15)
+    
+#     def on_start(self):
+#         self.client.post("/login", {
+#             "username": "test_user",
+#             "password": ""
+#         })
+    
+#     @task
+#     def index(self):
+#         self.client.get("/")
+#         self.client.get("/static/assets.js")
+        
+#     @task
+#     def about(self):
+#         self.client.get("/about/")
