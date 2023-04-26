8836774ea4605ac8e4d602c3c5a0b610a93da65b
tajbaevshamsiddin <tajbaevsh@gmail.com>
2023-04-26 16:08:02 +0500

    commit

--- locustfile.py
+++ locustfile.py
@@ -1,33 +1,16 @@
-# Imports classes from Locust
 from locust import HttpUser, task 
 
-# Instantiate a new virtual user
-class HelloWorldUser(HttpUser): 
-    # This tells locust to treat the method below 
-    # as something the virtual user would do
-    @task 
-    # Define a new method
-    def hello_world(self): 
-        # This method will run an HTTP GET request on the path `/` 
-        # of the site you are testing
-        self.client.post("/todos/", {
-            
-        }) 
 
-# class WebsiteUser(HttpUser):
-#     wait_time = between(5, 15)
+class LoadTesting(HttpUser):
+    wait_time = between(5, 15)
     
-#     def on_start(self):
-#         self.client.post("/login", {
-#             "username": "test_user",
-#             "password": ""
-#         })
+    @task
+    def on_start(self):
+        self.client.post("/login", {
+            "username": "test_user",
+            "password": ""
+        })
     
-#     @task
-#     def index(self):
-#         self.client.get("/")
-#         self.client.get("/static/assets.js")
-        
-#     @task
-#     def about(self):
-#         self.client.get("/about/")
+    @task
+    def about(self):
+        self.client.get("/about/")
