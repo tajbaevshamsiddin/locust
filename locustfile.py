a3a1fb54054015f30bd06e3bde476bbf19c2fbde
tajbaevshamsiddin <tajbaevsh@gmail.com>
2023-05-01 20:24:18 +0500

    qweq

--- locustfile.py
+++ locustfile.py
@@ -2,13 +2,17 @@ from locust import HttpUser, task, between
 
 class MyUser(HttpUser):
     wait_time = between(1, 5)
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgyOTUxNzkwLCJpYXQiOjE2ODI5NTExOTAsImp0aSI6IjExMWNiZWY0MTQ0ZjQ4NDVhNzI3ODM2MGVjZmUyMWU3IiwidXNlcl9pZCI6ImY3OTNkNDg3LTMyMWYtNDU5Yy1iZjJhLTg4YjhlNmVjOGVhZCJ9.QDzjlbndYJvYf1d-hXYNitXiZb7aPR_fpDtS7yQ2lDA'}
+    # headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer '}
+
+    # @task
+    # def post_request(self):
+    #     payload = {"name": "test"}
+    #     response = self.client.post("/api/niche/", json=payload, headers=self.headers)
+    #     if response.status_code == 201:
+    #         print("Post request sent successfully")
+    #     else:
+    #         print("Error sending post request")
 
     @task
-    def post_request(self):
-        payload = {"name": "test"}
-        response = self.client.post("/api/niche/", json=payload, headers=self.headers)
-        if response.status_code == 200:
-            print("Post request sent successfully")
-        else:
-            print("Error sending post request")
+    def about(self):
+        self.client.get("/api/niche/")
