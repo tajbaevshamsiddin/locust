5033969c38550d819c9055cc53e0094cc236d3b4
tajbaevshamsiddin <100567773+tajbaevshamsiddin@users.noreply.github.com>
2023-05-02 19:06:40 +0500

    Update locustfile.py
    change code

--- locustfile.py
+++ locustfile.py
@@ -3,16 +3,16 @@ from locust import HttpUser, task, between
 class MyUser(HttpUser):
     wait_time = between(3, 10)
     token = ""
-    # headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token) }
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token) }
 
-    # @task
-    # def post_request(self):
-    #     payload = {"name": "test"}
-    #     response = self.client.post("/api/niche/", json=payload, headers=self.headers)
-    #     if response.status_code == 201:
-    #         print("Post request sent successfully")
-    #     else:
-    #         print("Error sending post request")
+    @task
+    def post_request(self):
+        payload = {"name": "test"}
+        response = self.client.post("/api/niche/", json=payload, headers=self.headers)
+        if response.status_code == 201:
+            print("Post request sent successfully")
+        else:
+            print("Error sending post request")
 
     @task
     def about(self):
